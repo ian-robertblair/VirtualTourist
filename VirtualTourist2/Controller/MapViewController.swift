4907bca79d0f5a187cdeb6a389dec217b2ee616d
Ian Robert Blair <ian.robertblair@outlook.com>
2021-11-30 09:57:15 +0800

    Add Extension for MKMapView, functions for Fetch, and PicturesViewSegue

--- VirtualTourist2/Controller/MapViewController.swift
+++ VirtualTourist2/Controller/MapViewController.swift
@@ -6,24 +6,95 @@
 //
 
 import UIKit
+import MapKit
+import CoreLocation
+import CoreData
+import OSLog
 
 class MapViewController: UIViewController {
 
+    // MARK: -  Properties
+    let defaultLog = Logger()
+    var dataManager:NSManagedObjectContext!
+    var databaseLocations = [NSManagedObject]()
+    var location = ""
+    
+    // MARK: -  Outlets
+    @IBOutlet weak var map: MKMapView!
+    
+    
+    // MARK: -  Life Cycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        map.delegate = self
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        dataManager = appDelegate.persistentContainer.viewContext
+        fetchData()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        for eachLocation in databaseLocations {
+            let point = MKPointAnnotation()
+            point.title = eachLocation.value(forKey: "title") as? String
+            let longitude = eachLocation.value(forKey: "longitude") as! Double
+            let latitude = eachLocation.value(forKey: "latitude") as! Double
+            point.coordinate = CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
+            self.map.addAnnotation(point)
+        }
     }
-    */
+    
+    //MARK: -  Functions
+    func fetchData() {
+        let fetchRequest: NSFetchRequest<NSFetchRequestResult> = NSFetchRequest(entityName: "Pin")
+        do {
+            let result = try dataManager.fetch(fetchRequest)
+            databaseLocations = result as! [NSManagedObject]
+            defaultLog.info("Database Locations Fetched.")
+            for eachLocation in databaseLocations {
+                let name = eachLocation.value(forKey: "locationName") as! String
+                let longitude = eachLocation.value(forKey: "longitude") as! Double
+                let latitude = eachLocation.value(forKey: "latitude") as! Double
+                defaultLog.info("Database Entry.  Name: \(name)  Longitude: \(longitude)   Latitude: \(latitude)")
+            }
+            
+        } catch {
+            defaultLog.info("Failed to retrieve locations from database.  \(error.localizedDescription)")
+        }
+    }
+    
+    //MARK: - Preare for Segue
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "PictureViewSegue" {
+            let controller = segue.destination as! PicturesViewController
+            controller.location = location
+            controller.dataManager = dataManager
+        }
+    }
+    
+    //MARK: -  Actions
+    
+}
 
+//MARK: - Extensions
+extension MapViewController: MKMapViewDelegate {
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+        guard annotation is MKPointAnnotation else {return nil}
+        let identifier = "Annotation"
+        var annotationView = mapView.dequeueReusableAnnotationView(withIdentifier: identifier)
+        if annotationView == nil {
+            annotationView = MKPinAnnotationView(annotation: annotation, reuseIdentifier: identifier)
+            annotationView!.canShowCallout = true
+            annotationView!.rightCalloutAccessoryView = UIButton(type: .infoLight)
+        } else {
+            annotationView!.annotation = annotation
+        }
+        return annotationView
+    }
+    
+    func mapView(_ mapView: MKMapView, annotationView view: MKAnnotationView, calloutAccessoryControlTapped control: UIControl) {
+        performSegue(withIdentifier: "savedPicturesSegue", sender: self)
+    }
 }
--- VirtualTourist2/Controller/PicturesViewController.swift
+++ VirtualTourist2/Controller/PicturesViewController.swift
@@ -6,24 +6,34 @@
 //
 
 import UIKit
+import MapKit
+import CoreLocation
+import CoreData
+import OSLog
 
 class PicturesViewController: UIViewController {
 
+    
+    // MARK Properties
+    var dataManager:NSManagedObjectContext!
+    var location = ""
+    
+    
+    // MARK Outlets
+    
+    // MARK Life Cycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
     }
     
+    // MARK Functions
+    
+    
+    // MARK Actions
+    
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
